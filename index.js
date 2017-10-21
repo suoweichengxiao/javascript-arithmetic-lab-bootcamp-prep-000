a4c952c2e2cff2b217386775d02c4737ee7aa848
Joe Dettmar <joedettmar@gmail.com>
2017-10-21 08:34:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,3 @@ function divide(a, b) {return a / b}
 function inc(n) {return ++n}
 function dec(n) {return --n}
 function makeInt(n) {return parseInt(n)}
-function makeInt(n, b) {return parseInt(114, 10)}
