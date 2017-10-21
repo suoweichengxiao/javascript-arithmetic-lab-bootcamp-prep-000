f624677b557ea958172fc1c837f4f840e068027c
Joe Dettmar <joedettmar@gmail.com>
2017-10-21 08:37:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ function inc(n) {return ++n}
 function dec(n) {return --n}
 function makeInt(n) {return parseInt(n)}
 function makeInt(n) {return parseInt(n, 10)}
+function preserveDecimal(n) {return parseFloat(n)}
