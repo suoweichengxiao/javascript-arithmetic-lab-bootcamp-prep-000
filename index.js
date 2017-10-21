4ea54074b21445540ac168d97ac2654ffa34b825
Joe Dettmar <joedettmar@gmail.com>
2017-10-21 08:22:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ function add(a, b) {return a + b}
 function subtract(a, b) {return a - b}
 function multiply(a, b) {return a * b}
 function divide(a, b) {return a / b}
-function inc(n) {return (n++)}
+function inc(n)
+ {var number = n
+   return (n++)}
 function dec(n) {return (n--)}
